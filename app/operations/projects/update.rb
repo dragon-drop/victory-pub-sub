09ef4a3b1e6010dbe3b82d7d1c05cee4a84aadc4
adamdawkins <adam@dragondrop.uk>
2024-09-17 23:31:40 +0100

    Refactor Projects::Update

--- app/operations/projects/update.rb
+++ app/operations/projects/update.rb
@@ -14,20 +14,21 @@ module Projects
     def call
       Action.create(klass: 'Projects::Update', action: 'call')
       updated = project.update(changes)
-      if updated
-        publish(:project_updated, project)
-        if project.saved_change_to_status?
-          publish("project_#{project.status.downcase.parameterize.underscore}".to_sym,
-                  project)
-        end
-      else
-        publish(:project_not_updated, project)
-      end
+      success if updated
 
       updated
     end
 
     private
+
+    def success
+      publish(:project_updated, project)
+      publish(project_status_event_key, project) if project.saved_change_to_status?
+    end
+
+    def project_status_event_key
+      "project_#{project.status.downcase.parameterize.underscore}".to_sym
+    end
 
     attr_reader :project, :changes
   end
