14b40659037f7b23cff52d08d9c96ad068cf4f90
adamdawkins <adam@dragondrop.uk>
2024-09-17 23:56:08 +0100

    Replace broadcast in live finance statuses

--- app/modules/sales/broadcasts/projects/live_finance.rb
+++ app/modules/sales/broadcasts/projects/live_finance.rb
@@ -5,6 +5,10 @@ module Sales
         def self.add(_project)
           Action.create(klass: 'Sales::Broadcasts::Projects::LiveFinance', action: 'add(project)')
         end
+
+        def self.replace(_project)
+          Action.create(klass: 'Sales::Broadcasts::Projects::LiveFinance', action: 'replace(project)')
+        end
       end
     end
   end
--- app/modules/sales/listeners/project_listener.rb
+++ app/modules/sales/listeners/project_listener.rb
@@ -5,6 +5,25 @@ module Sales
         Action.create(klass: 'Sales::Listeners::ProjectListener', action: 'project_live_financing')
 
         Sales::Broadcasts::Projects::LiveFinance.add(project)
+        Sales::Broadcasts::Projects::LiveFinance.replace(project)
+      end
+
+      def project_finance_pre_approval(project)
+        Action.create(klass: 'Sales::Listeners::ProjectListener', action: 'project_finance_pre_approval')
+
+        Sales::Broadcasts::Projects::LiveFinance.replace(project)
+      end
+
+      def project_resolved(project)
+        Action.create(klass: 'Sales::Listeners::ProjectListener', action: 'project_resolved')
+
+        Sales::Broadcasts::Projects::LiveFinance.replace(project)
+      end
+
+      def project_pending_approval(project)
+        Action.create(klass: 'Sales::Listeners::ProjectListener', action: 'project_pending_approval')
+
+        Sales::Broadcasts::Projects::LiveFinance.replace(project)
       end
     end
   end
