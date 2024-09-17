4c2c069888e9b1e6505279b32aa4a9e41616b4ff
adamdawkins <adam@dragondrop.uk>
2024-09-17 20:26:30 +0100

    Make call an instance method

--- app/operations/projects/update.rb
+++ app/operations/projects/update.rb
@@ -1,7 +1,17 @@
 module Projects
   class Update
     include Wisper::Publisher
+
     def self.call(project, changes)
+      new(project, changes).call
+    end
+
+    def initialize(project, changes)
+      @project = project
+      @changes = changes
+    end
+
+    def call
       updated = project.update(changes)
       if updated
         publish(:project_updated, project)
@@ -11,5 +21,9 @@ module Projects
 
       updated
     end
+
+    private
+
+    attr_reader :project, :changes
   end
 end
