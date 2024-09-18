5e6c3257103b2a119e7e12560c2b32f70b5f605a
adamdawkins <adam@dragondrop.uk>
2024-09-18 11:07:47 +0100

    Add projects index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :projects, only: %i[show update]
+  resources :projects, only: %i[index show update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
