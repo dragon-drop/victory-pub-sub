cd5ff0de5e36a18f2bf358fa295010fab7481218
adamdawkins <adam@dragondrop.uk>
2024-09-18 11:08:42 +0100

    Remove actions index, make homepage project index

--- app/controllers/actions_controller.rb
+++
@@ -1,6 +0,0 @@
-class ActionsController < ApplicationController
-  # GET /actions
-  def index
-    @actions = Action.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :projects, only: %i[index show update]
+  resources :projects, only: %i[show update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root 'actions#index'
+  root 'projects#index'
 end
