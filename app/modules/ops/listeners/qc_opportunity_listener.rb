db45b6afaed9dd86b736187c3500a942ebfedaf7
adamdawkins <adam@dragondrop.uk>
2024-09-25 11:41:35 +0100

    Update last qc opportunity when appointment status changes

--- app/modules/ops/listeners/qc_opportunity_listener.rb
+++ app/modules/ops/listeners/qc_opportunity_listener.rb
@@ -5,6 +5,12 @@ module Ops
         Action.create(actionable: project, klass: 'Ops::Listeners::QcOpportunityListener', action: 'project_updated')
         Ops::Operations::Projects::QcOpportunities::UpdateLast.call(project)
       end
+
+      def appointment_status_changed(appointment)
+        Action.create(actionable: appointment, klass: 'Ops::Listeners::QcOpportunityListener',
+                      action: 'appointment_status_changed')
+        Ops::Operations::Projects::QcOpportunities::UpdateLast.call(appointment.project)
+      end
     end
   end
 end
